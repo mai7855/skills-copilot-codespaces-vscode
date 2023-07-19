aca3f594e8db79195babb7d35ef25ddba4e7f1e3
mai7855 <sam7855@gmail.com>
2023-07-19 15:35:39 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,13 @@
+function skillsMember() {
+    var member = {
+        name: "John Doe",
+        age: 30,
+        address: {
+            street: "50 Main St",
+            city: "Boston",
+            state: "MA"
+        },
+        skills: ["js", "html", "css"]
+    }
+    return member;
+}
